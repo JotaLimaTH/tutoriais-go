a7383915cd1b502449fcf5a363234d199e0d79c1
JotaLimaTH <123770747+JotaLimaTH@users.noreply.github.com>
2025-04-02 09:42:56 -0300

    Início do package runtime

--- ex012/main.go
+++ ex012/main.go
@@ -6,7 +6,12 @@ import (
 )
 
 func main() {
+	var memStats runtime.MemStats
+	runtime.ReadMemStats(&memStats)
+	// Constantes do package run time
 	fmt.Println(runtime.Compiler) // Compilador
 	fmt.Println(runtime.GOOS) // Sistema operacional
 	fmt.Println(runtime.GOARCH) // Arquitetura do processador
+	fmt.Println("Número de CPUs: ", runtime.NumCPU())
+	fmt.Println("Memória alocada: ", memStats.Alloc, "bytes") // Retorna em bytes
 }
