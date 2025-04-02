8f659457ec2530148daa535cfdb819b200dd8093
JotaLimaTH <123770747+JotaLimaTH@users.noreply.github.com>
2025-04-02 09:42:34 -0300

    Início do package runtime. Formatos binários, hexadecimal e decimal.

--- ex011/main.go
+++ ex011/main.go
@@ -6,6 +6,13 @@ func main() {
 	var nome string
 	var inteiro int = 5
 	var booleano bool = true
+	const (
+		_ = iota
+		a = iota
+		_ = iota
+		b = iota
+		c = iota * 5
+	) // Iota
 	string_muito_loka := fmt.Sprint("Bom dia. ", "Como vai você?") // Isso vai retornar uma string
 	fmt.Print("Isso vai printar sem criar uma nova linha a mais")
 	fmt.Println("Isso vai criar uma linha a mais.")
@@ -16,4 +23,7 @@ func main() {
 
 	fmt.Sscanf("Loucura 12 false", "%s %d %t", &string_muito_loka, &inteiro, &booleano) // Isso vai pegar o conteúdo separado por espaço e vai passar o valor para as variáveis.
 	fmt.Printf("%s %d %t", string_muito_loka, inteiro, booleano)
+
+	fmt.Printf("Decimal: %d\nBinário: %b\nHexadecimal: %#x\n", inteiro, inteiro, inteiro)
+	fmt.Printf("Constantes iota:\n\n%d\n%d\n%d", a, b, c)
 }
