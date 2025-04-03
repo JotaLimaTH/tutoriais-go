161a7c5407a477a7dcd7c8ac9e77d93eca484f48
JotaLimaTH <123770747+JotaLimaTH@users.noreply.github.com>
2025-04-03 10:14:16 -0300

    Package image. Salvamento de arquivo

--- ex013/main.go
+++ ex013/main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"fmt"
 	"image"
-	_ "image/png"
+	"image/png"
 	"os"
+	"image/color"
 )
 
 func main(){
@@ -21,4 +22,12 @@ func main(){
 	}
 	fmt.Println("Formato da imagem: ", format)
 	fmt.Printf("Dimensões: %dx%d\n", img.Bounds().Dx(), img.Bounds().Dy())
+
+	img_2 := image.NewRGBA(image.Rect(0, 0, 25, 25))
+	img_2.Set(10, 10, color.NRGBA{R: 255, G: 0, B: 100})
+
+	file_2, _ := os.Create("saida.png")
+	defer file_2.Close()
+
+	png.Encode(file_2, img_2)
 }
